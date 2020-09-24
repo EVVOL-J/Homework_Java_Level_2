f1973b3eb06acd328fbd14b27e3a4319b04cdff8
evvol <Keshavoloshin@mail.ru>
2020-09-24 18:41:21 +0300

    Lesson2 fixed

--- src/main/java/Lesson2/MyArraySizeException.java
+++ src/main/java/Lesson2/MyArraySizeException.java
@@ -1,8 +1,7 @@
 package Lesson2;
 
 public class MyArraySizeException extends RuntimeException{
-    private int numberCols;
-    private int numberRows;
+
     public MyArraySizeException(String string){
         System.out.printf(string);
     }
