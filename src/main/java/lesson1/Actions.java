4f71c867d9e7459c5dd4539b6e8ae0d261c7e226
evvol <Keshavoloshin@mail.ru>
2020-09-24 18:40:02 +0300

    Lesson2 del lesson1

--- src/main/java/lesson1/Actions.java
+++
@@ -1,9 +0,0 @@
-package Lesson1;
-
-public interface Actions {
-
-   int run();
-   int jump();
-
-   String infoName();
-}
--- src/main/java/lesson1/Obstacles.java
+++
@@ -1,6 +0,0 @@
-package Lesson1;
-
-public interface Obstacles {
-    boolean overcameObstacles(Actions actions);
-
-}
